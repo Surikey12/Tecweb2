1e2aeda979de6ad4c2cd76ad64450f572f547472
Surikey <surikey.hipolito@alumno.buap.mx>
2024-09-04 22:13:13 -0600

    Ejercicios 3 y 4 agregados correctamente

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -21,6 +21,7 @@
 
         print_r($variables);
         echo '<br>';
+
         #Ejercicio 2
         $a = "PHP server"; 
         $b = &$a; 
@@ -32,6 +33,54 @@
         print_r($c);
         echo '<br>';
         echo 'Este fragmento de código está utilizando referencias lo que permite que dos variables<br> apunten a la misma ubicación en memoria, de modo que un cambio en una de ellas afecta a la otra.';
+
+        echo '<br><br>';
+        #Ejercicio 3
+        $a = "PHP5"; 
+        print_r($a);
+        echo '<br>';
+        $z[] = &$a; 
+        print_r($z);
+        echo '<br>';
+        $b = "5a version de PHP"; 
+        print_r($b);
+        echo '<br>';
+        $c = $b*10;
+        print_r($c); 
+        echo '<br>';
+        $a .= $b; 
+        print_r($a);
+        echo '<br>';
+        $b *= $c; 
+        print_r($b);
+        echo '<br>';
+        $z[0] = "MySQL"; 
+        print_r($z[0]);
+        echo '<br><br>';
+
+        #Ejercicio 4
+        $a = "PHP5"; 
+        echo $GLOBALS["a"];
+        echo '<br>';
+        $z[] = &$a; 
+        foreach ($GLOBALS['z'] as $index => $value) {
+            echo "Valor de \$z[$index]: " . $value . "<br>";
+        }
+        echo '<br>';
+        $b = "5a version de PHP"; 
+        echo $GLOBALS["b"];
+        echo '<br>';
+        $c = $b*10;
+        echo $GLOBALS["c"]; 
+        echo '<br>';
+        $a .= $b; 
+        echo $GLOBALS["a"];
+        echo '<br>';
+        $b *= $c; 
+        echo $GLOBALS["b"];
+        echo '<br>';
+        $z[0] = "MySQL"; 
+        echo $GLOBALS["z"][0];
     ?>
 </body>
 </html>
