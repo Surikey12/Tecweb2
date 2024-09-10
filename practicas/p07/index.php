3c6afd656d627be78a02c3b3564faa79e8c0dc79
Surikey <surikey.hipolito@alumno.buap.mx>
2024-09-10 14:42:53 -0600

    Cambios agregados correctamente

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -6,27 +6,10 @@
     <title>Práctica 4</title>
 </head>
 <body>
-    <h2>Ejercicio 1</h2>
-    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
-    <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
-        }
-    ?>
-
-    <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
+    
+    <h2>Calcular multiplos de 5 y 7</h2>
+    <form action="http://localhost/tecweb/practicas/p07/src/funciones.php?numero=10" method="post">
+        Ingresa un número: <input type="text" name="numero"><br>
         <input type="submit">
     </form>
     <br>
