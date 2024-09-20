8ab502106b9b9b09cb2c9d7ea144449df69a97f6
Surikey <surikey.hipolito@alumno.buap.mx>
2024-09-20 00:03:18 -0600

    Cambios agregados correctamente

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,150 +1,47 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="UTF-8" />
-    <title>Manejo de variable en PHP</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Manejo de variable en PHP</title>
 </head>
 <body>
-    <?php
-
-        #ejercicio 1
-        $variables = [
-            '$_myvar => valida',  
-            '$_7var => valida',  
-            'myvar => invalida',
-            '<br>', 
-            '$myvar => valida',  
-            '$var7 => valida',  
-            '$_element1 => valida', 
-            '$house*5 => invalida'
-        ];
-
-        print_r($variables);
-        echo '<br>';
-
-        #Ejercicio 2
-        $a = "PHP server"; 
-        $b = &$a; 
-        $c = &$a;
-        print_r($a);
-        echo '<br>';
-        print_r($b);
-        echo '<br>';
-        print_r($c);
-        echo '<br>';
-        echo 'Este fragmento de código está utilizando referencias lo que permite que dos variables<br> apunten a la misma ubicación en memoria, de modo que un cambio en una de ellas afecta a la otra.';
-
-        echo '<br><br>';
-        #Ejercicio 3
-        $a = "PHP5"; 
-        print_r($a);
-        echo '<br>';
-        $z[] = &$a; 
-        print_r($z);
-        echo '<br>';
-        $b = "5a version de PHP"; 
-        print_r($b);
-        echo '<br>';
-        $c = $b*10;
-        print_r($c); 
-        echo '<br>';
-        $a .= $b; 
-        print_r($a);
-        echo '<br>';
-        $b *= $c; 
-        print_r($b);
-        echo '<br>';
-        $z[0] = "MySQL"; 
-        print_r($z[0]);
-        echo '<br><br>';
-
-        #Ejercicio 4
-        $a = "PHP5"; 
-        echo $GLOBALS["a"];
-        echo '<br>';
-        $z[] = &$a; 
-        foreach ($GLOBALS['z'] as $index => $value) {
-            echo "Valor de \$z[$index]: " . $value . "<br>";
-        }
-        echo '<br>';
-        $b = "5a version de PHP"; 
-        echo $GLOBALS["b"];
-        echo '<br>';
-        $c = $b*10;
-        echo $GLOBALS["c"]; 
-        echo '<br>';
-        $a .= $b; 
-        echo $GLOBALS["a"];
-        echo '<br>';
-        $b *= $c; 
-        echo $GLOBALS["b"];
-        echo '<br>';
-        $z[0] = "MySQL"; 
-        echo $GLOBALS["z"][0];
-        echo '<br><br>';
-
-        #Ejercicio 5
-        $a = "7 personas"; 
-        $b = (integer) $a; 
-        $a = "9E3"; 
-        $c = (double) $a; 
-
-        print_r($a); echo '<br>';
-        print_r($b); echo '<br>';
-        print_r($c); echo '<br>';
-
-        #Ejercicio 6
-        $a = "0";  
-        $b = "TRUE"; 
-        $c = FALSE;  
-        $d = ($a || $b);  
-        $e = ($a && $c);  
-        $f = ($a xor $b);
-        echo "Valores booleanos con var_dump():<br>";
-        var_dump($a);  // Mostrará el tipo y valor de $a
-        echo '<br>';
-        var_dump($b);  // Mostrará el tipo y valor de $b
-        echo '<br>';
-        var_dump($c);  // Mostrará el tipo y valor de $c
-        echo '<br>';
-        var_dump($d);  // Mostrará el tipo y valor de $d
-        echo '<br>';
-        var_dump($e);  // Mostrará el tipo y valor de $e
-        echo '<br>';
-        var_dump($f);  // Mostrará el tipo y valor de $f
-        echo '<br>';
-        // Para mostrar los valores booleanos con echo, transformamos los valores de $c y $e
-
-        // Usamos la función boolval() para asegurarnos de que sean booleanos
-        // Usamos un operador ternario para transformarlos en algo que se pueda mostrar con echo
-        echo "<br>Mostrar valores booleanos de forma legible:<br>";
-        echo "\$c: " . ($c ? 'TRUE' : 'FALSE') . "<br>";  // Mostrará 'FALSE'
-        echo "\$e: " . ($e ? 'TRUE' : 'FALSE') . "<br>";  // Mostrará 'FALSE'
-
-        # EJERCICIO 7
-
-        echo '<br>';
-        if (isset($_SERVER['SERVER_SOFTWARE'])) {
-            echo 'Versión de Apache: ' . $_SERVER['SERVER_SOFTWARE'] . '<br>';
-        } else {
-            echo 'No se pudo determinar la versión de Apache.<br>';
-        }
-
-        echo 'Versión de PHP: ' . phpversion() . '<br>';
-
-
-        if (isset($_SERVER['SERVER_SOFTWARE'])) {
-            echo 'Nombre del sistema operativo del servidor: ' . php_uname() . '<br>';
-        } else {
-            echo 'No se pudo determinar el nombre del sistema operativo del servidor.<br>';
-        }
-
-
-        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-            echo 'Idioma del navegador (cliente): ' . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . '<br>';
-        } else {
-            echo 'No se pudo determinar el idioma del navegador.<br>';
-        }
-    ?>
+<p>Array</p>
+<pre>
+(
+[0] => $_myvar => valida
+[1] => $_7var => valida
+[2] => myvar => invalida
+[3] =>
+[4] => $myvar => valida
+[5] => $var7 => valida
+[6] => $_element1 => valida
+[7] => $house*5 => invalida
+)
+</pre>
+<p>PHP server</p>
+<p>Este fragmento de código está utilizando referencias, lo que permite que dos variables apunten a la misma ubicación en memoria, de modo que un cambio en una de ellas afecta a la otra.</p>
+<p>PHP5</p>
+<pre>
+Array
+(
+[0] => PHP5
+)
+</pre>
+<p>5a versión de PHP</p>
+<p>50</p>
+<p>5050</p>
+<p>25502500</p>
+<p>MySQL</p>
+<p>9000</p>
+<p>Valores booleanos con var_dump():</p>
+<p>bool(false)</p>
+<p>Mostrar valores booleanos de forma legible:</p>
+<p>$c: FALSE</p>
+<p>$e: FALSE</p>
+<p>Versión de Apache: Apache/2.4.58 (Win64) OpenSSL/3.1.3 PHP/8.0.30</p>
+<p>Versión de PHP: 8.0.30</p>
+<p>Nombre del sistema operativo del servidor: Windows NT SURIKEY 10.0 build 22631 (Windows 10) AMD64</p>
+<p>Idioma del navegador (cliente): es-MX,es;q=0.9,en-US;q=0.8,en;q=0.7</p>
 </body>
 </html>
